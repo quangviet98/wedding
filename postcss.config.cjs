ebe35bf5e86fe641db5872d0c7f1d238be104131
viet.trinh <viet.trinhquang@haravan.com>
2025-04-17 18:57:15 +0700

    up

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
     purgecss({
       content: ['./src/**/*.{html,js,ts,tsx}'],
       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+      safelist: [
+        /^body$/, /^h1$/, /^h2$/, /^h3$/, /^h4$/, /^h5$/, /^h6$/, /^p$/, /^a$/, /^ul$/, /^ol$/, /^li$/, /^button$/, /^img$/, /^html$/
+      ], // Add more tags if needed
     }),
     cssnano({
       preset: 'default',
