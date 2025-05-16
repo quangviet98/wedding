120e67dc248a5b1b89a651c576944a80b45e927b
viet.trinh <viet.trinhquang@haravan.com>
2025-05-16 15:37:40 +0700

    up

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,5 +1,7 @@
+import { motion } from 'framer-motion';
 import Image38 from '../assets/Image-38.png';
 import Image41 from '../assets/Image-41.png';
+import useWindowSize from '../hooks/useWindowSize';
 import ShortCouple from './ShortCouple';
 import './Stories.scss';
 
@@ -32,6 +34,7 @@ const stories: StoryItem[] = [
 ];
 
 function Stories() {
+  const { width } = useWindowSize();
   return (
     <div className="stories-section">
       <ShortCouple />
@@ -39,38 +42,108 @@ function Stories() {
       <div className="stories-container">
         <div className="stories-header">
           <h2 className="link-service w-inline-block heading-service">Câu chuyện của chúng mình</h2>
-
-          {/* <h2 className="section-title">Câu chuyện của chúng mình</h2> */}
           <p className="paragraph-service">Những dấu mốc đáng nhớ trên hành trình yêu thương</p>
         </div>
         <div className="stories-timeline">
-          {stories.map((story, index) => (
-            <div
+          {stories.map((story) => (
+            <motion.div
               key={story.date}
               className={'story-item'}
-              style={{ animationDelay: `${index * 0.3}s` }}
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true, amount: 0.3 }}
+              transition={{
+                duration: 0.8,
+                ease: 'easeOut',
+              }}
             >
               <div className="story-date">{story.date}</div>
-              <img src={Image38} className="story-flower-1" />
-              <img src={Image41} className="story-flower-2" />
+              <motion.img
+                src={Image38}
+                className="story-flower-1"
+                initial={{ opacity: 0, scale: 0.5 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ once: false, margin: '-100px', amount: 'some' }}
+                transition={{
+                  duration: 0.8,
+                  delay: 0.5,
+                  ease: 'easeOut',
+                  scale: {
+                    delay: 0.5,
+                    type: 'spring',
+                    stiffness: 100,
+                    damping: 15,
+                    duration: 0.6, // Longer duration for scale
+                  },
+                }}
+                style={{ rotate: 50 }} // Move the rotation to inline style
+              />
+              <motion.img
+                src={Image41}
+                className="story-flower-2"
+                initial={{ opacity: 0, scale: 0.5 }}
+                whileInView={{ opacity: 0.9, scale: 1 }}
+                viewport={{ once: false, margin: '-100px', amount: 'some' }}
+                transition={{
+                  duration: 0.8,
+                  delay: 0.5,
+                  ease: 'easeOut',
+                  scale: {
+                    delay: 0.5,
+                    type: 'spring',
+                    stiffness: 100,
+                    damping: 15,
+                    duration: 0.6, // Longer duration for scale
+                  },
+                }}
+                style={{ rotate: width > 767 ? 37 : -12 }} // Move the rotation to inline style
+              />
 
               <div className="story-content">
-                <div className="story-content__left">
+                <motion.div
+                  className="story-content__left"
+                  initial={
+                    width > 767
+                      ? { x: '-100%', opacity: 0, y: 0, scale: 0.7 }
+                      : { x: 0, opacity: 0, y: 50, scale: 0.7 }
+                  }
+                  whileInView={{ x: 0, opacity: 1, y: 0, scale: 1 }}
+                  viewport={{ once: false, amount: 'some' }}
+                  transition={{
+                    duration: 1,
+                    ease: 'anticipate',
+                    opacity: { duration: 0.6 },
+                  }}
+                >
                   <img
                     src={
                       'https://cdn.prod.website-files.com/6109925e44b6ab8a7601f26a/610c995bd5f573418e62eb65_img_11.jpg'
                     }
                     alt={story.title}
                   />
-                </div>
-                <div className="story-content__right">
+                </motion.div>
+                <motion.div
+                  className="story-content__right"
+                  initial={
+                    width > 767
+                      ? { x: '100%', opacity: 0, y: 0, scale: 0.2 }
+                      : { x: 0, opacity: 0, y: 50, scale: 0.7 }
+                  }
+                  whileInView={{ x: 0, opacity: 1, y: 0, scale: 1 }}
+                  viewport={{ once: false, amount: 'some' }}
+                  transition={{
+                    duration: 0.6,
+                    ease: 'anticipate',
+                    opacity: { duration: 0.6 },
+                  }}
+                >
                   <div className="story-content__right-wrapper">
                     <h3 className="story-title">{story.title}</h3>
                     <p className="story-description">{story.description}</p>
                   </div>
-                </div>
+                </motion.div>
               </div>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
